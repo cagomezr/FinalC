b9b9f298eedaffbf5da08d0dc0c068444f746d18
cagomezr <carlos.a.gomez.romero@gmail.com>
2019-11-27 13:48:00 -0500

    Update LineManager.cpp
    added   validate task
    Skeleton of  constructor

--- FinalC/LineManager.cpp
+++ FinalC/LineManager.cpp
@@ -2,7 +2,34 @@
 
 Linemanager::Linemanager(const std::string filename, std::vector<Task*>& taskBuilder, std::vector<CustomerOrder>& Customerorders)
 {
-	
+	std::move(Customerorders.begin(), Customerorders.end(), std::back_inserter(ToBeFilled));
+	std::copy(taskBuilder.begin(), taskBuilder.end(), std::back_inserter(AssemblyLine));
+	ifstream file(filename);
+	if (!file)
+		throw string("Unable to open [") + filename + "] file.";
+	string record;
+	Utilities pointersmanager
+	while (!file.eof())
+	{
+		std::getline(file, record);
+		Task* begin;
+		Task* end;
+		size_t startCode = 0u;
+		bool moreContent = true;
+		auto lambdafind = [&](string dats) {
+			//add search logic			
+			// returns the task data address;
+		};
+		
+		std::string return1 = pointersmanager.extractToken(record, startCode, moreContent);
+		if (morecontent) {
+			std::string return2 = pointersmanager.extractToken(record, startCode, moreContent);
+			begin = lambdafind(return1);
+			end = lambdafind(return2);
+			begin->setNextTask(end);
+		}
+	}
+
 }
 
 bool Linemanager::run(std::ostream&)
@@ -20,4 +47,8 @@ void Linemanager::displayCompleted(std::ostream& out) const
 
 void Linemanager::validateTasks() const
 {
+	auto runfunc = [&](Task& single) {
+		single.validate(std::cout);
+	};
+	std::for_each(AssemblyLine.begin(), AssemblyLine.end(), runfunc);
 }
